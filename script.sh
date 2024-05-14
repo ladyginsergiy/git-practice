5b5756d6d4f11593f8cc9869fc77b3aff2c386de
Sergiy Ladygin <sergiyladygin@gmail.com>
2024-05-14 11:03:31 +0300

    Added backup_script.sh file, modified script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 # !/bin/bash
-cat /etc/passwd | cut -d : -f1 | xargs -I % crontab -l -u %
+# Modified by Sergiy 14.05.2024
+
+cat /etc/passwd | cut -d : -f1 | xargs -I % crontab -l -u % | grep -i "root"
+echo "IPADDR1" >> /etc/network/interfaces
